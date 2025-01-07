5e988128e6dbaab8a2e55e47ae4b7b61d13b505b
DJDrama <putzzang@naver.com>
2025-01-07 14:56:59 +0900

    create a segmented control

---
+++ SwiftUI-Cookbook/Pickers/SegmentedControlView.swift
@@ -0,0 +1,28 @@
+//
+//  SegmentedControlView.swift
+//  SwiftUI-Cookbook
+//
+//  Created by Dongjun Lee on 1/7/25.
+//
+
+import SwiftUI
+
+struct SegmentedControlView: View {
+    @State private var selection: String = "Student"
+    var body: some View {
+        VStack {
+            Picker("Select", selection: $selection) {
+                // tag must match the selection value!
+                Text("Student").tag("Student")
+                Text("Staff").tag("Staff")
+                Text("Faculty").tag("Faculty")
+            }.pickerStyle(SegmentedPickerStyle())
+            
+            Text(selection)
+        }
+    }
+}
+
+#Preview {
+    SegmentedControlView()
+}
